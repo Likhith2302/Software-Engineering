52accbf04c028fb9651d9fa4748107380c547c0b
Likhith2302 <145013661+Likhith2302@users.noreply.github.com>
2024-12-11 10:07:26 +0530

    Update and rename Weather_modelling.ipynb to Weather_modelling.py

--- Weather_modelling.py
+++ Weather_modelling.py
@@ -1,20 +1,10 @@
-# Stage 3: Reading from a File
-
 def quadratic_weather_model(a, b, c, x):
-    # Calculate the value using the quadratic equation
     y = a * x**2 + b * x + c
     return y
-
-# Read coefficients and input value from the file
 with open('coeff.txt', 'r') as file:
     for line in file:
-        # Split the line into values and convert them to float
         a, b, c, x = map(float, line.split())
-
-        # Calculate the result using the quadratic model
         result = quadratic_weather_model(a, b, c, x)
-
-        # Print the result
         print(f"For x={x}, the result is: {result}")
 
 
